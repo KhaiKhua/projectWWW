becf667fba6fa6ce1c487e5e78d4ae04d3a99eb7
KhaiKhua <quangkhai24022002@gmail.com>
2023-10-01 13:54:16 +0700

    Week03

--- WWW_week01/src/main/java/com/example/demo_week_01/resource/AcountRepository.java
+++ WWW_week01/src/main/java/com/example/demo_week_01/resource/AcountRepository.java
@@ -40,17 +40,17 @@ public class AcountRepository {
         return false;
     }
     public List<Account> getlisstAccount (){
-        return em.createQuery("Select a form Account a").getResultList();
+        return em.createQuery("Select a from Account a").getResultList();
     }
     public  Account find (String id){
-        Account acc = (Account) em.createQuery("Select a form Account a where a.ccount_id ="+id).getSingleResult();
+        Account acc = (Account) em.createQuery("Select a from Account a where a.ccount_id ="+id).getSingleResult();
         return acc;
     }
     public List<Account > findAccByOneRole(){
-        return em.createQuery("Select a form Account a join GrantAccess g on a.ccount_id= g.account_id group by a having Count(g.role.role_id)=1 ").getResultList();
+        return em.createQuery("Select a from Account a join GrantAccess g on a.ccount_id= g.account_id group by a having Count(g.role.role_id)=1 ").getResultList();
     }
     public Account findAccByName(String Name){
-        return (Account) em.createQuery("Select a form Account a join GrantAccess g on a.ccount_id= g.account_id where a.account_id= "+Name).getSingleResult();
+        return (Account) em.createQuery("Select a from Account a join GrantAccess g on a.ccount_id= g.account_id where a.account_id= "+Name).getSingleResult();
     }
 
 
