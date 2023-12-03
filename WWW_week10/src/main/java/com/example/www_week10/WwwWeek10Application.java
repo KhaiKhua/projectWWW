c2e81a96be8a317b81fe72446e7c96ae245721e5
KhaiKhua <quangkhai24022002@gmail.com>
2023-12-03 16:01:42 +0700

    week10

---
+++ WWW_week10/src/main/java/com/example/www_week10/WwwWeek10Application.java
@@ -0,0 +1,13 @@
+package com.example.www_week10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwWeek10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwWeek10Application.class, args);
+	}
+
+}
---
+++ WWW_week10/src/test/java/com/example/www_week10/WwwWeek10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.www_week10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwWeek10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
